a18fbd0573664c87a165e9580f532d47f5ca36be
Ozoda Ernazarova <ozoshka1207@gmail.com>
2024-06-06 12:57:18 +0500

    feat: add game page

--- src/pages/GamesPage.jsx
+++ src/pages/GamesPage.jsx
@@ -1,9 +1,15 @@
 export default function GamesPage() {
   return (
-    <div className="container">
-      <h2 className="text-xl md:text-2xl font-bold leading-7 text-gray-900 uppercase text-center">
-        GamesPage
+    <div className="container overflow-hidden py-10">
+      <h2 className="mb-16 text-3xl font-bold text-gray-900 leading-8 text-center">
+        Berilgan rasmlardan qushlarni va ayna shu qushning tuxumini mosini
+        topishingiz kerak.
       </h2>
+      <embed
+        src="https://biologiya-diplom.vercel.app/games/"
+        // src="http://localhost:3000/games/"
+        className="w-full h-[80vh] overflow-hidden"
+      />
     </div>
   );
 }
