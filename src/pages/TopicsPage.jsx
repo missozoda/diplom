e537c77e7f98ecd4c69ac27ec22d047939d36254
Ozoda Ernazarova <ozoshka1207@gmail.com>
2024-06-12 12:35:33 +0500

    fix topics page

--- src/pages/TopicsPage.jsx
+++ src/pages/TopicsPage.jsx
@@ -2,8 +2,8 @@ export default function TopicsPage() {
   return (
     <div className="container overflow-hidden py-10">
       <embed
-        // src="https://biologiya-diplom.vercel.app/topics/"
-        src="http://localhost:3000/topics/"
+        src="https://biologiya-diplom.vercel.app/topics/"
+        // src="http://localhost:3000/topics/"
         className="w-full min-h-[80vh] h-full overflow-hidden"
       />
     </div>
